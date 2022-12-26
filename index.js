103d0cc4b1f1572b783da492932b04eda81368df
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2022-12-25 23:38:29 -0500

    I scrapped the pages of standard, vintage, legacy, and modern. Saved the deck archetype names and the url to get each deck list

--- index.js
+++ index.js
@@ -2,4 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const puppeteer = require('puppeteer');
+const fs = require('fs/promises');
+const { arrayBuffer } = require('stream/consumers');
 
+let cardJson = new Object();
+async function start(){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.mtgtop8.com/event?e=40289&d=499906&f=ST");
+    //await page.screenshot({path: "standardmtg.png", fullPage: true});
+
+    const cards = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('.deck_line')).map(x => x.textContent);
+    });
+
+    cardJson.cards = cards;
+
+    await browser.close();
+}
+
+app.get('/', (req, res) => {
+    //res.send(typeof cardJson);
+    res.send(cardJson);
+})
+
+app.listen(port, () => {
+    console.log("Listening is port $1", [port]);
+})
+
+start();
---
+++ legacy.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+
+async function start(){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.mtgtop8.com/format?f=LE");
+    
+    /*Have to extract all of the links for each deck archtype in standard and save it in a JSON object
+    Don't forget to add the main url to the url saved in the json data when sending it out
+    Ex. https://www.mtgtop8.com/ + url
+    They all share the class name S14
+    */
+    let deckArchtypes = await page.evaluate( () => {
+        const results = [];
+        const mainUrl = 'https://www.mtgtop8.com/';
+        const item = document.querySelectorAll('div.S14 > a[href] ');
+        item.forEach(element => {
+            results.push({
+                deckName:  element.textContent, //Saves the deck archetype name 
+                url: mainUrl + element.getAttribute('href') //Saves the url link of the deck archetype
+            });
+        });
+        return results;
+    });
+
+    /*
+    Tried to extract the deck popularity might do that later on.
+    let deckPopularity = await page.evaluate( () => {
+        const list = [];
+        const items = document.querySelectorAll('');
+        items.forEach(element => {
+            list.push({
+                //deckName:  element.getElementsByTagName('a').href,
+                text: element.textContent
+            });
+        });
+        return list;
+    });
+    */
+    await console.log(deckArchtypes);
+    //await console.log(deckPopularity);
+
+    await browser.close();
+}
+
+start();
