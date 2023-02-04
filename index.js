b8e57df8f3a4691f230bed950ae8d26edcd2269a
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2023-02-03 21:26:29 -0500

    Removed the practice puppeteer code

--- index.js
+++ index.js
@@ -2,23 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const puppeteer = require('puppeteer');
+const test = require('./Decks/standard.js');
 
 
-let cardJson = new Object();
-async function start(){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://www.mtgtop8.com/event?e=40289&d=499906&f=ST");
-    //await page.screenshot({path: "standardmtg.png", fullPage: true});
-
-    const cards = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('.deck_line')).map(x => x.textContent);
-    });
-
-    cardJson.cards = cards;
-
-    await browser.close();
-}
 
 app.get('/', (req, res) => {
     //res.send(typeof cardJson);
