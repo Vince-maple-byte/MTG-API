775bb26e59cf0cb62c671e9639146acd23c9210f
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2023-04-01 01:45:58 -0400

    Made some comments

--- routes/Decks/alchemyRoutes.js
+++ routes/Decks/alchemyRoutes.js
@@ -2,12 +2,19 @@ const express = require('express');
 const alchemy = require('../../Decks/alchemy.js');
 const router = express.Router();
 
+//Getting the magic the gathering deck
 router.get('/', (req, res) => {
     async function launch(){
         const deck = await alchemy.alchemy();
         res.send(deck);
     }
     launch();
-})
+});
+
+//Need to make an update deck
+
+//Need to make a delete deck
+
+//Need to make a create deck into the database
 
 module.exports = router;
