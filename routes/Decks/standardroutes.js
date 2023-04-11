240362af5acde1a74c042f33646a0f89ba338695
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2023-04-11 00:45:36 -0400

    .

--- routes/Decks/standardroutes.js
+++ routes/Decks/standardroutes.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const standard  = require('../../Decks/standard.js');
+const Deck = require('../../mongoose/database.js');
 const router = express.Router();
 
 //Standard api routes
-router.get('/', (req, res) => {
-    async function launch(){
-        const deck = await standard.standard();
-        res.send(deck);
-    }
-    launch();
-})
+router.route('/')
+    .get((req, res) => {
+        async function launch(){
+            const deck = await Deck.find({format: 'Standard'});
+            res.send(deck);
+        }
+        launch();
+    })
+    .post((req, res) => {
+        async function launch(){
+            const deck = await standard.standard();
+            const createDeck = new Deck({
+                deckName: deck.deckName,
+                format: 'Standard',
+                formatVersion: deck.format,
+                url: deck.url,
+                cards: deck.cards
+            })
+            await createDeck.save();
+            console.log("Decks have been created");
+        }
+        launch().catch(err => console.log(err))
+    })
+    .delete((req, res) => {
+        async function launch(){
+            const deck = await Deck.deleteMany({format: 'Standard'});
+            res.send("Everything in the standard format has been deleted");
+        }
+        launch();
+    })
 
 router.get('/:standardFormatId', (req, res) => {
     async function launch(){
