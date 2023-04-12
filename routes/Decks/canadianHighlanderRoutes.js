78df62a5eef1fe68bb443e364cbb5e4104625032
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2023-04-12 00:17:15 -0400

    I finished the CRUD api routes and the database  of the canadian highlander format

--- routes/Decks/canadianHighlanderRoutes.js
+++ routes/Decks/canadianHighlanderRoutes.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const canadianHighlander = require('../../Decks/canadianhighlander.js');
+const Deck = require('../../mongoose/database.js');
 
 
-router.get('/', (req, res) => {
-    async function launch(){
-        const deck = await canadianHighlander.canadianHighlander();
-        res.send(deck);
-    }
-    launch();
-})
+router.route('/')
+    .get((req, res) => {
+        async function launch(){
+            const deck = await Deck.find({format: 'Canadian Highlander'});
+            res.send(deck);
+        }
+        launch();
+    })
+    .post((req, res) => {
+        async function launch(){
+            const deck = await canadianHighlander.canadianHighlander();
+            for(let i = 0; i < deck.length; i++){
+                const createDeck = new Deck({
+                    deckName: deck[i].deckName,
+                    format: 'Canadian Highlander',
+                    formatVersion: 'allCanadianHighlanderDecks',
+                    url: deck[i].cards
+                })
+                await createDeck.save();
+            }
+            res.send("Decks have been created");
+        }
+        launch();
+    })
+    .delete((req, res) => {
+        async function launch(){
+            await Deck.deleteMany({format: 'Canadian Highlander'});
+            res.send("Everything in the canadian highlander format has been deleted");
+        }
+        launch();
+    })
 
 module.exports = router;
