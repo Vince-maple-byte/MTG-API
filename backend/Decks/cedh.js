ab2da51c32ee4a3c0ab1fb2da748fbf341cf803b
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2023-05-25 17:13:21 -0400

    Was able to have cedh to work with the initial deck archtype url and saving it to the database. I was also able to fix the delete route for the cedh

--- backend/Decks/cedh.js
+++ backend/Decks/cedh.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const Deck = require("../mongoose/database");
 
 async function start(){
     const browser = await puppeteer.launch();
@@ -25,25 +26,18 @@ async function start(){
         return results;
     });
     
-    //This solves the issue of getting the links to each individual deck
-    //Might only do one deck for each archetype since this is kind of overkill and could run into issues with server timing in the future.
-    let decksUrl = [];
-    for (let i = 0; i < deckArchtypes.length; i++) {
-        const url = deckArchtypes[i].url;
-        await page.goto(`${url}`);
-        decksUrl[i] = await page.evaluate( () => {
-            const results = [];
-            const mainUrl = 'https://www.mtgtop8.com/';
-            const selection = document.querySelector('td:nth-child(2) > form > table > tbody  td:nth-child(2) > a');
-            //const percent = document.querySelectorAll('div:nth-child(2) > div:nth-child(2) > div:nth-child(1)');
-            results.push(
-                    mainUrl + selection.getAttribute('href') //Saves the url link of the deck archetype
-            );
-            return results;
+    for(let i = 0; i < deckArchtypes.length; i++){
+        const createDeck = await new Deck({
+            deckName: deckArchtypes[i].deckName,
+            format: 'cEDH',
+            formatVersion: null,
+            url: deckArchtypes[i].url,
+            cards: null
         })
+        await createDeck.save();
     }
     
-
+    /*
     //Save the cards in an array with the number of each card in the deck(Complete)
     let cards = [];
     for (let i = 0; i < decksUrl.length; i++) {
@@ -69,11 +63,39 @@ async function start(){
             cards: cards[index] // Cards
         }
     }
+    */
     await browser.close();
-    return finalDeck;
+    //return decksUrl;
 }
 
-start();
+async function deckUrl(){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const deckArchtypes = await Deck.find({'format':'cEDH'}); //Looks for the decks in the with the format cEDH
+    //This solves the issue of getting the links to each individual deck
+    //Might only do one deck for each archetype since this is kind of overkill and could run into issues with server timing in the future.
+    let decksUrl = []; //Creates an array for the deck url
+    for (let i = 0; i < deckArchtypes.length; i++) {
+        const url = deckArchtypes[i].url; //Get the deck archtype url
+        const id = deckArchtypes[i]._id; //Get the id of the object
+        await page.goto(`${url}`); //Go to the deck archtype url
+        //Where we are going to get the urls for the decks
+        decksUrl[i] = await page.evaluate( () => {
+            const results = [];
+            const mainUrl = 'https://www.mtgtop8.com/';
+            const selection = document.querySelector('td:nth-child(2) > form > table > tbody  td:nth-child(2) > a');//The query selector for the deck url element
+            //const percent = document.querySelectorAll('div:nth-child(2) > div:nth-child(2) > div:nth-child(1)');
+            results.push(
+                    mainUrl + selection.getAttribute('href') //Saves the url link of the deck archetype
+            );
+            return results;
+        })
+        const filter = {'_id': new Object(`${id}`)}; //This is going to filter the update to the id of the deck url we want to change
+        const update = {'url': `${deckUrl[i][0]}`}; //The url we want to change
+        const change = await Deck.updateOne({filter, update}); //This does the update
+    }
+}
 
-module.exports = {start};
+
+module.exports = {start, deckUrl};
 
--- backend/routes/Decks/cedhRoutes.js
+++ backend/routes/Decks/cedhRoutes.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const cedh = require('../../Decks/cedh.js');
+const Deck = require('../../mongoose/database.js')
 
 router.get('/', (req, res) => {
     async function launch(){
         const deck = await cedh.start();
         res.send(deck);
+    }
+    launch();
+})
+
+router.post('/', (req, res) => {
+    async function launch(){
+        await cedh.start();
+        //await cedh.deckUrl();
+    }
+    launch();
+})
+
+router.delete('/', (req, res) => {
+    async function launch(){
+        try {
+            const deck = await Deck.deleteMany({format: 'cEDH'});
+            res.status(200).send("Everything in the cEDH format has been deleted");
+        } catch (error) {
+            res.status(400);
+            res.send(error + " This is bad");
+        }
     }
     launch();
 })
