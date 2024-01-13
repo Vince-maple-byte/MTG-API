b83f567003d165c9f10bf943ebaf824b0971c2a9
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2024-01-13 00:14:14 -0500

    Started the frontend of the magic the gathering api project. I just finished the navbar. Need to work on the home page next.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './nav/Navbar' 
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/nav/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useState } from 'react';
+import './Navbar.css'
+
+export default function Navbar(){
+    const [search, setSearch] = useState("");
+
+    //We make an api call to send them to the deck page of what they searched for
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log(search);
+    }
+
+    function handleChange(event) {
+        const searchData = event.target.value;
+        setSearch(prevSearch => searchData)
+    }
+
+    return (
+        <div className='nav'>
+            <div className='nav--logo'>
+                <img src="\resources\lotus.png" alt="" className='nav--image' />
+                <h4>MTG API</h4>
+                <form className='nav--search' onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder='Search a deck'
+                        onChange={handleChange}
+                        name='searchData'
+                        value={search}
+                    />
+                    <button>Search</button>
+                </form>
+
+            </div>
+
+            <div className='nav--format_list'>
+                <h4>Home</h4>     
+                <h4>Formats</h4>
+                <h4>Cards</h4>
+                <h4>More</h4>
+            </div>
+        
+        </div>
+    )
+
+}
