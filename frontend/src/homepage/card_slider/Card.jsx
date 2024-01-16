c578c86af6da1e9b3c07652ebbd412ce4d622f13
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2024-01-16 02:06:04 -0500

    Figured out the main design and logic of the Card slider in the home page. Just need to connect it to the database for the deck names and card images.

--- frontend/src/homepage/card_slider/Card.jsx
+++ frontend/src/homepage/card_slider/Card.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './Card.css'
-import { FaCircleArrowRight } from "react-icons/fa6";
-import { FaCircleArrowLeft } from "react-icons/fa6";
+
 
 export default function Card(props){
     return (
         <>  
-            <div className="card--elements">
-                <FaCircleArrowLeft />
-                <Link to={`/format`} className='card'>
-                    <img src={props.cards[0].img} alt="" className='card--img'/>
-                    <div className="card--name">{props.cards[0].name}</div>
-                </Link>
-                <FaCircleArrowRight />    
-            </div>
-            
+            <Link to={`/format`} className='card'>
+                    <img src={props.img} alt="" className='card--img'/>
+                    <div className="card--name">{props.name}</div>
+            </Link>          
         </>
     )
 }
--- frontend/src/homepage/card_slider/CardSlider.jsx
+++ frontend/src/homepage/card_slider/CardSlider.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import Card from "./Card"
+import Card from "./Card.jsx";
+import { FaCircleArrowRight } from "react-icons/fa6";
+import { FaCircleArrowLeft } from "react-icons/fa6";
+import './CardSlider.css';
+import { useState } from "react";
 
 export default function CardSlider(){
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     const cards = [
         {
             img:'../../../resources/lotus.png',
@@ -24,11 +30,29 @@ export default function CardSlider(){
             name:'Bored Lotus'
         }
     ]
+
+    const onChange = (e, direction) => {
+        if(direction === 'left' && currentIndex === 0){
+            setCurrentIndex(cards.length - 1);
+        }else if(direction === 'left'){
+            setCurrentIndex(prevIndex => prevIndex - 1);
+        }else if(direction === 'right' && currentIndex === cards.length - 1){
+            setCurrentIndex(0);
+        }else{
+            setCurrentIndex(prevIndex => prevIndex + 1);
+        }
+    } 
     return (
         <>
-            <Card 
-                cards={cards}
-            />
+            <div className="card--elements">
+                <FaCircleArrowLeft className="left--arrow" onClick={e => onChange(e, 'left')}/>
+                <Card  
+                    img={cards[currentIndex].img}
+                    name={cards[currentIndex].name}
+                />
+                <FaCircleArrowRight className="right--arrow" onClick={e => onChange(e, 'right')}/>
+            </div>
+            
         </>
     )
 }
