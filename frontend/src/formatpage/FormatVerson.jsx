261449252f91ade584a57b851674502e117c6bf2
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2024-02-10 00:08:26 -0500

    Was able to figure out how to make a basic table with react-table. Next up need to know how to do pagination, and then we figure out how to sort columns by user clicking on the column header.

--- frontend/src/formatpage/FormatVerson.jsx
+++ frontend/src/formatpage/FormatVerson.jsx
@@ -1,19 +1,87 @@
 import React from 'react';
 import './formatversion.css';
-import {Link, useParams} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+import tableData from './tables/tabledata.js';
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+  } from '@tanstack/react-table';
+
+  
+const columnHelper = createColumnHelper();
+
+const columns = [
+    columnHelper.accessor('deckImage',{
+        header: 'Deck Image',
+        cell: url => <img src={url.getValue()} alt="" className='format--version--image'/> //Cell changes the values in the cell
+    }),
+    columnHelper.accessor('deckName', {
+        id: 'deckName',
+        header: 'Deck Name',
+        cell: info => info.getValue(),
+    }),
+    columnHelper.accessor('deckPercentage', {
+        id: 'deckPercentage',
+        header: 'Deck Percentage',
+        cell: info => info.getValue(),
+    }),
+    columnHelper.accessor('formatVersion', {
+        id: 'formatVersion',
+        header: 'Format Version',
+        cell: info => info.getValue(),
+    }),
+];
 
 
-export default function FormatVersion(props){
+
+export default function FormatVersion(){
     const {id} = useParams();
+    const [data, setData] = React.useState(tableData)
+
+    
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+      });
 
     return (
         <>
             <h1 className='format'>Format: {id}</h1>
-            <h1 className='meek'>JJJ</h1>
-            <h1> Crack Cocaine</h1>
-            <div className="modern--table">
-                
+            <div className='format--table'>
+                <table>
+                    <thead>
+                        {table.getHeaderGroups().map(headerGroup => (
+                            <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext()
+                                    )}
+                                </th>
+                            ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody>
+                        {table.getRowModel().rows.map(row => (
+                            <tr key={row.id}>
+                                {row.getVisibleCells().map(cell => (
+                                    <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
+            
         </>
     )
 }
---
+++ frontend/src/formatpage/tables/tabledata.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        deckName: "Rakdos Aggro",
+        deckImage: "https://www.mtgtop8.com/metas_thumbs/1105.jpg",
+        deckPercentage: "14 %",
+        formatVersion: "All Alchemy Decks",  
+    }, 
+    {
+        deckName: "Weenie White ",
+        deckImage: "https://www.mtgtop8.com//metas_thumbs/1129.jpg",
+        deckPercentage: "11 %",
+        formatVersion: "All Alchemy Decks",
+    },
+    {
+        deckName: "Naya Aggro",
+        deckImage: "https://www.mtgtop8.com//metas_thumbs/1090.jpg",
+        deckPercentage: "11 %",
+        formatVersion: "All Alchemy Decks"
+    }
+]
+
+export default data;
