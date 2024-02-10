f7217fbe395a62a0c0ff67d5b24e3e36d09ba72d
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2024-02-10 04:27:44 -0500

    Was able to add the pagination in the table. Just need to add the sorting for the columns and the option panel for which format version to show.

--- frontend/src/formatpage/FormatVerson.jsx
+++ frontend/src/formatpage/FormatVerson.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './formatversion.css';
-import {useParams} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import tableData from './tables/tabledata.js';
+import axios from 'axios';
 import {
     createColumnHelper,
     flexRender,
     getCoreRowModel,
     useReactTable,
+    getPaginationRowModel,
   } from '@tanstack/react-table';
 
   
@@ -38,14 +40,40 @@ const columns = [
 
 export default function FormatVersion(){
     const {id} = useParams();
-    const [data, setData] = React.useState(tableData)
+    const [data, setData] = React.useState([])
 
     
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        debugTable: true,
       });
+
+    React.useEffect(() => {
+        const fetchData = async() => {
+            try {
+                console.log(id.toLowerCase())
+                let response = '';
+                if(id.toLocaleLowerCase() === 'duel commander'){
+                    response = await axios.get(`http://localhost:3000/duel-commander`);
+                }
+                else if(id.toLocaleLowerCase() === 'canadian highlander'){
+                    response = await axios.get(`http://localhost:3000/canadianHighlander`);
+                }
+                else{
+                    response = await axios.get(`http://localhost:3000/${id.toLowerCase()}`);
+                }
+                
+                console.log(response.data)
+                setData(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        fetchData();
+    }, [])
 
     return (
         <>
@@ -56,7 +84,7 @@ export default function FormatVersion(){
                         {table.getHeaderGroups().map(headerGroup => (
                             <tr key={headerGroup.id}>
                             {headerGroup.headers.map(header => (
-                                <th key={header.id}>
+                                <th key={header.id} >
                                 {header.isPlaceholder
                                     ? null
                                     : flexRender(
@@ -70,16 +98,67 @@ export default function FormatVersion(){
                     </thead>
                     <tbody>
                         {table.getRowModel().rows.map(row => (
-                            <tr key={row.id}>
+                            <tr 
+                                key={row.id} 
+                            >
                                 {row.getVisibleCells().map(cell => (
-                                    <td key={cell.id}>
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </td>
+                                    <td key={cell.id} className='cell--rows'>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>     
                                 ))}
                             </tr>
                         ))}
                     </tbody>
                 </table>
+                <div className="format--pagination">
+                    <button 
+                        className="pagination--firstPage"
+                        onClick={() => table.setPageIndex(0)}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {'<<'}
+                    </button>
+                    <button 
+                        className="pagination--previousPage"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {'<'}
+                    </button>
+                    <button 
+                        className="pagination--nextPage"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {'>'}
+                    </button>
+                    <button 
+                        className="pagination--lastPage"
+                        onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {'>>'}
+                    </button>
+                    <span>
+                        <div>Page</div>
+                        <strong>
+                            {table.getState().pagination.pageIndex + 1} of{" "}
+                            {table.getPageCount()}
+                        </strong>
+                    </span>
+                    <select 
+                        value={table.getState().pagination.pageSize}
+                        onChange={e => {
+                            table.setPageSize(Number(e.target.value))
+                        }}
+                    >
+                        {[10, 20, 30, 40, 50].map(pageSize => (
+                            <option key={pageSize} value={pageSize}>
+                                Show {pageSize}
+                            </option>
+                        ))}
+                    </select>
+                </div>
             </div>
             
         </>
