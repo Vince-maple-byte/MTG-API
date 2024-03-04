3197de89a19a0cc47cb8c7df00383ea037088c5e
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2024-03-04 00:56:21 -0500

    Fixed the issue with the home pages next up the search bar

--- frontend/src/homepage/card_slider/Card.jsx
+++ frontend/src/homepage/card_slider/Card.jsx
@@ -6,7 +6,9 @@ import './Card.css'
 export default function Card(props){
     return (
         <>  
-            <Link to={`/format`} className='card--div'>
+            <Link to=
+            {`/deck?deckName=${encodeURIComponent(props.name)}&format=${encodeURIComponent('Standard')}&formatVersion=${encodeURIComponent('standardLast2Months')}`}
+             className='card--div'>
                     <img src={props.img} alt="" className='card--img'/>
                     <div className="card--name">{props.name}</div>
             </Link>          
